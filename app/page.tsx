bbc4773dbc0ee13aa223acbfbd472cff871f9492
Hyuni Kim <spaceship00@naver.com>
2024-07-13 02:44:40 +0000

    Add 레슨 신청하기 button

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     <>
       <Header />
       <ImageSlide srcs={[]} />
+      <div className="text-center m-3">
+        <button className="py-2 px-4 bg-blue-300 rounded-lg text-white transition duration-300 ease-in-out hover:brightness-110">
+          레슨 신청하기
+        </button>
+      </div>
     </>
   );
 }
