bcd9fccecc2281b5faa6b6f4c250be2ebd7f20c2
Hyuni Kim <spaceship00@naver.com>
2024-07-14 15:27:20 +0000

    Show slot dialog edit mode

--- app/manage/slots/SlotDialog.tsx
+++ app/manage/slots/SlotDialog.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import Dialog from '@/app/components/Dialog';
 import TimeSlot from '@/app/data/TimeSlot';
 import SlotDialogField from './SlotDialogField';
+import { useState } from 'react';
 
 interface Props {
   slot: TimeSlot;
@@ -8,16 +11,28 @@ interface Props {
 }
 
 export default function SlotDialog({ slot, onClose }: Props) {
+  const [isEditMode, setIsEditMode] = useState(false);
+
   return (
     <Dialog>
       <div className="w-48 text-sm mx-4 mt-4 mb-2">
         <h1 className="text-base mb-2">{slot.title}</h1>
-        <SlotDialogField title="코치" initialValue={slot.coach} />
-        <SlotDialogField title="가격" initialValue={slot.price} suffix="원" />
+        <SlotDialogField
+          title="코치"
+          initialValue={slot.coach}
+          isEditMode={isEditMode}
+        />
+        <SlotDialogField
+          title="가격"
+          initialValue={slot.price}
+          suffix="원"
+          isEditMode={isEditMode}
+        />
         <SlotDialogField
           title="정원"
           initialValue={slot.capacity}
           suffix="명"
+          isEditMode={isEditMode}
         />
         <div className="mt-2">
           <span className="block">
@@ -26,13 +41,38 @@ export default function SlotDialog({ slot, onClose }: Props) {
           <span>{slot.students.join(', ')}</span>
         </div>
         <div className="text-right">
-          <button className="p-2 font-bold text-gray-500">수정</button>
-          <button
-            className="p-2 font-bold text-gray-500"
-            onClick={() => onClose(false)}
-          >
-            닫기
-          </button>
+          {!isEditMode && (
+            <>
+              <button
+                className="p-2 font-bold text-gray-500"
+                onClick={() => setIsEditMode(true)}
+              >
+                수정
+              </button>
+              <button
+                className="p-2 font-bold text-gray-500"
+                onClick={() => onClose(false)}
+              >
+                닫기
+              </button>
+            </>
+          )}
+          {isEditMode && (
+            <>
+              <button
+                className="p-2 font-bold text-gray-500"
+                onClick={() => setIsEditMode(false)}
+              >
+                취소
+              </button>
+              <button
+                className="p-2 font-bold text-gray-500"
+                onClick={() => onClose(false)}
+              >
+                완료
+              </button>
+            </>
+          )}
         </div>
       </div>
     </Dialog>
--- app/manage/slots/SlotDialogField.tsx
+++ app/manage/slots/SlotDialogField.tsx
@@ -2,22 +2,28 @@ interface Props {
   title: string;
   initialValue: string | number;
   suffix?: string;
+  isEditMode: boolean;
 }
 
 export default function SlotDialogField({
   title,
   initialValue,
   suffix,
+  isEditMode,
 }: Props) {
+
   return (
-    <div>
+    <div className='flex whitespace-nowrap'>
       <b className="mr-2">{title}</b>
-      <span>
-        {typeof initialValue === 'number'
-          ? initialValue.toLocaleString()
-          : initialValue}
-        {suffix && <>{suffix}</>}
-      </span>
+      {!isEditMode && (
+        <span className='flex-grow border-b border-transparent'>
+          {typeof initialValue === 'number'
+            ? initialValue.toLocaleString()
+            : initialValue}
+          {suffix && <>{suffix}</>}
+        </span>
+      )}
+      {isEditMode && <input className='flex-grow border-b' size={10} />}
     </div>
   );
 }
