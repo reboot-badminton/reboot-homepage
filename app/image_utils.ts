2b15613d873782fda56ab5c0b8e9694c9939dfa7
Hyuni Kim <spaceship00@naver.com>
2024-07-17 02:39:01 +0000

    Use basePath instead of images path

--- app/image_utils.ts
+++ app/image_utils.ts
@@ -1,5 +1,5 @@
 import nextConfig from '@/next.config.mjs';
 
 export function src(path: string) {
-  return (nextConfig.images?.path ?? '') + path;
+  return (nextConfig.basePath ?? '') + path;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@ const env = process.env.NODE_ENV;
 
 if (env === 'production') {
   nextConfig['basePath'] = '/reboot-homepage';
-  nextConfig['assetPrefix'] = '/reboot-homepage';
-  nextConfig['images'] = {
-    path: '/reboot-homepage',
-  };
 }
 
 export default nextConfig;
