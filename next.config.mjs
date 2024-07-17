21c9fa6d3e2a2481a007a7cfd7ea48d5757080ad
Hyuni Kim <spaceship00@naver.com>
2024-07-17 02:20:13 +0000

    Remove images loader

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ if (env === 'production') {
   nextConfig['basePath'] = '/reboot-homepage';
   nextConfig['assetPrefix'] = '/reboot-homepage';
   nextConfig['images'] = {
-    loader: 'imgix',
     path: 'https://reboot-badminton.github.io/reboot-homepage',
   };
 }
