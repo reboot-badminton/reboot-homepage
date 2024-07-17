6d7eaaab772f50be80c85ef130d79123647817f5
Hyuni Kim <spaceship00@naver.com>
2024-07-17 02:13:25 +0000

    Add images path in production

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const env = process.env.NODE_ENV;
 if (env === 'production') {
   nextConfig['basePath'] = '/reboot-homepage';
   nextConfig['assetPrefix'] = '/reboot-homepage/';
+  nextConfig['images'] = {
+    loader: 'imgix',
+    path: '/reboot-homepage',
+  };
 }
 
 export default nextConfig;
