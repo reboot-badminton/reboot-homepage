b74a1328fdab6399e3c4474e38c64cd88b72a52e
Justin <justinlee@unussystems.com>
2024-07-17 15:52:44 +0900

    #3 따옴표

--- app/components/MainCoaches.tsx
+++ app/components/MainCoaches.tsx
@@ -8,7 +8,7 @@ export default function MainCoaches() {
   const [isLeftExpanded, setIsLeftExpanded] = useState(true);
 
   const handleClick = (side: string) => {
-    setIsLeftExpanded(side == "left");
+    setIsLeftExpanded(side === 'left');
   };
 
   const leftClipPath = isLeftExpanded
@@ -27,7 +27,7 @@ export default function MainCoaches() {
           backgroundImage: `url(${src('/slide/1.jpeg')})`,
           clipPath: leftClipPath,
         }}
-        onClick={() => handleClick("left")}
+        onClick={() => handleClick('left')}
       >
         <div className="absolute top-2 left-2"><Image src={src('/antroke.jpg')} alt="an" width={48} height={48}/></div>
         {isLeftExpanded&&(<div className="absolute bottom-4 left-4 w-1/2 text-white">
@@ -42,13 +42,13 @@ export default function MainCoaches() {
           backgroundImage: `url(${src('/slide/2.jpeg')})`,
           clipPath: rightClipPath,
         }}
-        onClick={() => handleClick("right")}
+        onClick={() => handleClick('right')}
       >
         <div className="absolute top-2 right-2 backdrop-blur-sm"><Image src={src('/reboot-icon.png')} alt="yoon" width={48} height={48}/></div>
        {!isLeftExpanded&&( <div className="absolute bottom-4 right-4 w-1/2 text-white">
           <h2 className="text-2xl font-bold mb-2 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">윤 코치</h2>
-          <p className="">윤 코치에 대한 설명을 여기에 추가하세요.</p>
-          <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolorem, provident veritatis quos, nesciunt, officia explicabo consequuntur doloribus eaque saepe quidem! Accusamus, culpa? Molestiae soluta veniam earum, ullam adipisci ratione.</p>
+          <p>윤 코치에 대한 설명을 여기에 추가하세요.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolorem, provident veritatis quos, nesciunt, officia explicabo consequuntur doloribus eaque saepe quidem! Accusamus, culpa? Molestiae soluta veniam earum, ullam adipisci ratione.</p>
         </div>)}
       </div>
     </div>
