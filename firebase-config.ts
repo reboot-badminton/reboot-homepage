732218b30c948f60992fd68cb779ed94def3f426
Hyuni Kim <spaceship00@naver.com>
2024-07-17 01:45:09 +0000

    Share firebase config - web config is safe to share and is already publically accessible through web source anyway

---
+++ firebase-config.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyC-RV0DJURYoXuLGjgmD0Q8JRw-xGOW6P0',
+  authDomain: 'reboot-badminton.firebaseapp.com',
+  projectId: 'reboot-badminton',
+  storageBucket: 'reboot-badminton.appspot.com',
+  messagingSenderId: '922071552287',
+  appId: '1:922071552287:web:4f0f4747c31da46f60202a',
+  measurementId: 'G-K4T7BQQ6BF',
+};
