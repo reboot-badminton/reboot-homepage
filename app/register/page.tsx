ab096d8a253f69c76ce3e7d251447486460df246
Hyuni Kim <spaceship00@naver.com>
2024-07-18 07:46:38 +0000

    Add a button to go back after registration

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -7,6 +7,7 @@ import { firestore } from '../firebase/firebase';
 import { addDoc, collection } from 'firebase/firestore';
 import Dialog from '../components/Dialog';
 import { useRouter } from 'next/navigation';
+import { ConfirmDialogButton } from '../components/DialogButtons';
 
 export default function Register() {
   const router = useRouter();
@@ -42,19 +43,16 @@ export default function Register() {
     register().then(() => {
       setIsRegistering(false);
       setIsSuccess(true);
-      setTimeout(() => {
-        router.back();
-      }, 3000);
     });
   }, []);
 
   return (
-    <div className="p-2">
-      <p className="text-sm mb-4">
+    <div className='p-2'>
+      <p className='text-sm mb-4'>
         아래 양식을 채운 후, 신청하기 버튼을 눌러주세요. 매니저님 확인 후
         회신해드리겠습니다.
       </p>
-      <p className="text-xs mb-1 italic">
+      <p className='text-xs mb-1 italic'>
         별표 (*) 표시된 항목은 필수 입력 항목입니다.
       </p>
       {Object.entries(registration.current).map(([_, field]) => (
@@ -64,12 +62,20 @@ export default function Register() {
           showError={showError}
         />
       ))}
-      <button className="w-full mt-4" onClick={onSubmit}>
+      <button className='w-full mt-4' onClick={onSubmit}>
         신청하기
       </button>
-      {isRegistering && <Dialog text="신청중입니다" useDotAnimation={true} />}
+      {isRegistering && <Dialog text='신청중입니다' useDotAnimation={true} />}
       {isSuccess && (
-        <Dialog text="신청 완료했습니다." useDotAnimation={false} />
+        <Dialog text='신청 완료했습니다.' useDotAnimation={false}>
+          <div className='text-right pr-2'>
+            <ConfirmDialogButton
+              onClick={() => {
+                router.back();
+              }}
+            />
+          </div>
+        </Dialog>
       )}
     </div>
   );
