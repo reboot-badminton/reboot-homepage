cb91e781c86c42202fc4b131dcf09c64b0c07d81
LeeWonbae <judiking1@gmail.com>
2024-07-28 16:54:03 +0900

    Feature/ssr #2 loading

--- app/manage/registrations/page.tsx
+++ app/manage/registrations/page.tsx
@@ -30,51 +30,57 @@ export default async function ManageRegistrations() {
   return (
     <div className="p-4">
       <h1 className="text-2xl mb-4">등록된 신청서 관리</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {registrations?.map((registration, registrationIndex) => (
-          <div key={registration.id} className="p-4 rounded shadow">
-            <div>{'#' + (registrationIndex + 1)}</div>
-            <div className="p-2 text-sm whitespace-nowrap">
-              <div className="flex gap-4">
-                <div>이름 :</div>
-                <div>{registration.name ?? '-'}</div>
-              </div>
-              <div className="flex gap-4">
-                <div>성별 :</div>
-                <div>{registration.gender ?? '-'}</div>
-              </div>
-              <div className="flex gap-4">
-                <div>전화번호 :</div>
-                <div>{registration.phone ?? '-'}</div>
-              </div>
-              <div className="flex gap-4">
-                <div>생년월일 :</div>
-                <div>{formatDate(registration.birthday) ?? '-'}</div>
-              </div>
-              <div className="flex gap-4">
-                <div>급수 :</div>
-                <div>{registration.level ?? '-'}</div>
-              </div>
-              <div className="flex gap-4">
-                <div>목표 :</div>
-                <div>{registration.goal ?? '-'}</div>
-              </div>
-              <div className="w-full whitespace-normal flex gap-4">
-                <div className="whitespace-nowrap">하고 싶은 말 :</div>
-                <div>{registration.others ?? '-'}</div>
-              </div>
-              <div>
-                <div>신청시간 :</div>
-                <RegistrationTimeSlot
-                  initialRegistrationTimeSlots={registration.times}
-                  registrationId={registration.id}
-                  name={registration.name}
-                />
+      {!registrations?.length ? (
+        <div className="text-center text-gray-500">
+          등록된 신청서가 없습니다.
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {registrations.map((registration, registrationIndex) => (
+            <div key={registration.id} className="p-4 rounded shadow">
+              <div>{'#' + (registrationIndex + 1)}</div>
+              <div className="p-2 text-sm whitespace-nowrap">
+                <div className="flex gap-4">
+                  <div>이름 :</div>
+                  <div>{registration.name ?? '-'}</div>
+                </div>
+                <div className="flex gap-4">
+                  <div>성별 :</div>
+                  <div>{registration.gender ?? '-'}</div>
+                </div>
+                <div className="flex gap-4">
+                  <div>전화번호 :</div>
+                  <div>{registration.phone ?? '-'}</div>
+                </div>
+                <div className="flex gap-4">
+                  <div>생년월일 :</div>
+                  <div>{formatDate(registration.birthday) ?? '-'}</div>
+                </div>
+                <div className="flex gap-4">
+                  <div>급수 :</div>
+                  <div>{registration.level ?? '-'}</div>
+                </div>
+                <div className="flex gap-4">
+                  <div>목표 :</div>
+                  <div>{registration.goal ?? '-'}</div>
+                </div>
+                <div className="w-full whitespace-normal flex gap-4">
+                  <div className="whitespace-nowrap">하고 싶은 말 :</div>
+                  <div>{registration.others ?? '-'}</div>
+                </div>
+                <div>
+                  <div>신청시간 :</div>
+                  <RegistrationTimeSlot
+                    initialRegistrationTimeSlots={registration.times}
+                    registrationId={registration.id}
+                    name={registration.name}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
