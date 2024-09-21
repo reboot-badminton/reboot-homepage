3de82dcaa999c0e66cc5d1412463a550afb53222
Hyuni Kim <spaceship00@naver.com>
2024-09-21 01:28:56 +0000

    Add event slide

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Members from './members';
 import MainCoaches from './components/MainCoaches';
 import Introduction from './components/Introduction';
 
-const slideImages = Array.from({ length: 4 }, (_, index) => ({
-  src: `/slide/${index + 1}.jpeg`,
-  alt: `blurred slide Image ${index + 1}`,
+const slideImages = Array.from({ length: 5 }, (_, index) => ({
+  src: `/slide/${index}.jpeg`,
+  alt: `blurred slide Image ${index}`,
 }));
 
 export default function Home() {
