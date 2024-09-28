449a879c8f7e5d8d87c95bbe7a309c4af6c4344c
Hyuni Kim <spaceship00@naver.com>
2024-09-28 03:28:58 +0000

    Add user manage button

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -15,6 +15,12 @@ export default function Manage() {
       >
         레슨 신청 관리
       </Link>
+      <Link
+        href="/manage/users"
+        className="inline-block py-2 px-4 bg-pink-300 rounded-lg text-white transition duration-300 ease-in-out hover:brightness-110"
+      >
+        유저 관리
+      </Link>
     </div>
   );
 }
