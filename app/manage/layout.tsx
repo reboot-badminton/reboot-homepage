93ac7008cc41fbe66127bcb4513ccc9e9c8be8db
Hyuni Kim <spaceship00@naver.com>
2024-10-05 15:06:44 +0000

    Make pages under manage/ only accessible by admins and managers

---
+++ app/manage/layout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+import Authorized from '../components/Authorized';
+import { Role } from '@/firebase';
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <Authorized allowedRoles={[Role.ADMIN, Role.MANAGER]}>
+      {children}
+    </Authorized>
+  );
+}
