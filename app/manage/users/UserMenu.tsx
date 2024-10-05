060367b1fcaf3cf1b6bad82dbd784726272b9d15
Hyuni Kim <spaceship00@naver.com>
2024-10-05 17:47:11 +0000

    Use router.refresh after user role update again

--- app/manage/users/UserMenu.tsx
+++ app/manage/users/UserMenu.tsx
@@ -3,12 +3,15 @@
 import Menu, { MenuItem } from '@/app/components/Menu';
 import { firestore, Role, toRole } from '@/firebase';
 import { doc, updateDoc } from 'firebase/firestore';
+import { useRouter } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
 
 export default function UserMenu({ uid, role }: { uid: string, role?: Role }) {
+  const router = useRouter();
+
   const updateRole = useCallback(async (role: Role) => {
     await updateDoc(doc(firestore, 'users', uid), { role: toRole(role) });
-    location.reload();
+    router.refresh();
   }, [uid]);
 
   const [items, setItems] = useState<MenuItem[]>([]);
