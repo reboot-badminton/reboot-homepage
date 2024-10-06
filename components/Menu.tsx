21ddcd2333da3aa5a88a9cfd7a864253cc367ce1
judiking1 <judiking1@gmail.com>
2024-10-06 21:40:48 +0900

    커서모양

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -45,7 +45,7 @@ export default function Menu({ items }: { items: MenuItem[] }) {
       {isOpen && <div className="absolute top-4 right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
         {items.map(item => (
           <div className="py-1" role="none" key={`menu-item-${item.text}`}>
-            <div className="block px-4 py-2 text-sm" role="menuitem" id="menu-item-0"
+            <div className="block px-4 py-2 text-sm cursor-pointer hover:bg-slate-300" role="menuitem" id="menu-item-0"
               onClick={(e) => {
                 item.onClick();
                 setIsOpen(false);
