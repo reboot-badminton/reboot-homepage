8338f326feea112ebf9212447b0fe5e9f35e8bfe
Hyuni Kim <spaceship00@naver.com>
2024-10-06 06:03:35 +0000

    Add login success page after login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,6 @@ import { doc, getDoc } from 'firebase/firestore';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useCallback, useState } from 'react';
-import Authorized from '../Authorized';
 import { useDialog } from '../providers/DialogProvider';
 
 export default function Login() {
@@ -36,7 +35,7 @@ export default function Login() {
       onLoginSuccess();
     } else {
       showDialog({
-        title: '회원가입이 필요합니다',
+        title: '가입되어 있지 않은 이메일입니다',
         body: '서비스 이용을 위해 간편 회원가입을 하시겠습니까?',
         onCancel: async () => {
           signOut(getAuth(app));
@@ -52,39 +51,37 @@ export default function Login() {
   }, [isSignedUp, onLoginSuccess]);
 
   return (
-    <Authorized requiresSignOut={true}>
-      <main className="flex min-h-screen flex-col items-center justify-center p-8">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              로그인
-            </h1>
-            <div className="space-y-4 md:space-y-6">
-              <Authentication
-                emailVerificationText='이메일로 로그인'
-                googleVerificationText='구글 계정으로 로그인'
-                onUserSignedIn={onUserSignedIn} />
-              {error && (
-                <div
-                  className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                  role="alert"
-                >
-                  <span className="block sm:inline">{error}</span>
-                </div>
-              )}
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                계정이 없으신가요?
-                <Link
-                  href="/signup"
-                  className="ml-4 font-medium text-gray-600 hover:underline dark:text-gray-500"
-                >
-                  회원가입하기
-                </Link>
-              </p>
-            </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            로그인
+          </h1>
+          <div className="space-y-4 md:space-y-6">
+            <Authentication
+              emailVerificationText='이메일로 로그인'
+              googleVerificationText='구글 계정으로 로그인'
+              onUserSignedIn={onUserSignedIn} />
+            {error && (
+              <div
+                className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                role="alert"
+              >
+                <span className="block sm:inline">{error}</span>
+              </div>
+            )}
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              계정이 없으신가요?
+              <Link
+                href="/signup"
+                className="ml-4 font-medium text-gray-600 hover:underline dark:text-gray-500"
+              >
+                회원가입하기
+              </Link>
+            </p>
           </div>
         </div>
-      </main>
-    </Authorized>
+      </div>
+    </main>
   );
 }
--- components/authentication/EmailVerification.tsx
+++ components/authentication/EmailVerification.tsx
@@ -7,6 +7,7 @@ enum State {
   REQUEST,
   PENDING,
   VERIFICATION,
+  VERIFIED,
 }
 
 interface Props {
@@ -36,9 +37,9 @@ export default function EmailVerification({ onVerified, verificationText }: Prop
   const verify = useCallback(async () => {
     if (state !== State.VERIFICATION) return;
 
-    const userCredential = await signInWithEmailLink(getAuth(), email, window.location.href);
+    await signInWithEmailLink(getAuth(), email, window.location.href);
     window.localStorage.removeItem('emailForSignIn');
-    onVerified(userCredential.user);
+    setState(State.VERIFIED);
   }, [email]);
 
   const onSubmit = useCallback(() => {
@@ -46,8 +47,17 @@ export default function EmailVerification({ onVerified, verificationText }: Prop
       case State.REQUEST:
         sendVerification();
         break;
+      case State.PENDING:
+        const user = getAuth().currentUser;
+        if (user != null) {
+          onVerified(user);
+        }
+        break;
       case State.VERIFICATION:
         verify();
+        break;
+      case State.VERIFIED:
+        window.close();
         break;
     }
   }, [state, email, sendVerification, verify]);
@@ -66,7 +76,7 @@ export default function EmailVerification({ onVerified, verificationText }: Prop
 
   return (
     <div className="space-y-4 md:space-y-6">
-      <div>
+      {state !== State.VERIFIED && <div>
         <label
           htmlFor="email"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -82,17 +92,19 @@ export default function EmailVerification({ onVerified, verificationText }: Prop
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="email@naver.com"
           required
-          disabled={state === State.PENDING || isEmailFromLocalStorage}
+          disabled={state !== State.REQUEST || isEmailFromLocalStorage}
         />
-      </div>
+      </div>}
+      {state === State.PENDING && <div>인증 이메일을 발송했습니다.<br />인증 완료 후 아래 버튼을 눌러주세요.</div>}
+      {state === State.VERIFIED && <div>이메일 인증이 완료되었습니다.<br />이전 페이지로 돌아가주세요.</div>}
       <button
         onClick={onSubmit}
         className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
-        disabled={state === State.PENDING}
       >
         {state === State.REQUEST && <>{verificationText}</>}
-        {state === State.PENDING && <>인증 이메일을 발송했습니다</>}
+        {state === State.PENDING && <>인증 완료</>}
         {state === State.VERIFICATION && <>이메일 인증 완료하기</>}
+        {state === State.VERIFIED && <>창 닫기</>}
       </button>
     </div>
   );
