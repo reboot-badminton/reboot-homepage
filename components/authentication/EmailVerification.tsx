6d7c8ea3629a4b7da3225d036d4261e8fc208879
judiking1 <judiking1@gmail.com>
2024-10-06 15:39:22 +0900

    #1 로그인버튼 비활성화시 호버기능 비활성화, register의 버튼과 통일

--- components/authentication/EmailVerification.tsx
+++ components/authentication/EmailVerification.tsx
@@ -99,7 +99,7 @@ export default function EmailVerification({ onVerified, verificationText }: Prop
       {state === State.VERIFIED && <div>이메일 인증이 완료되었습니다.<br />이전 페이지로 돌아가주세요.</div>}
       <button
         onClick={onSubmit}
-        className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
+        className="w-full"
         disabled={!email}
       >
         {state === State.REQUEST && <>{verificationText}</>}
