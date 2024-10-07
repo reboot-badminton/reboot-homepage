72829f3892750952f792671a4a2944f46879b82b
CubeDr <spaceship00@naver.com>
2024-10-08 01:43:48 +0900

    Limit max width of verification page

--- app/verification/page.tsx
+++ app/verification/page.tsx
@@ -3,7 +3,7 @@ import Authentication from "@/components/authentication/Authentication";
 export default function Verification() {
   return (
     <main className="flex flex-col items-center justify-center p-8">
-      <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+      <div className="w-[420px] bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
             이메일 인증
