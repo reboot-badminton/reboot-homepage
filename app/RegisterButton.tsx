9b866e5f47641001fdc012e0f5c67d5a3f834098
Hyuni Kim <spaceship00@naver.com>
2024-10-07 09:28:47 +0000

    Hide the button border to the banned users

--- app/RegisterButton.tsx
+++ app/RegisterButton.tsx
@@ -8,10 +8,12 @@ export default function RegisterButton() {
   const { userData } = useAuth();
 
   return (
-    <div className="border-white border-2 text-xl mobile:px-4 mobile:py-2 px-8 py-4 transition-all hoverable:hover:scale-110 active:scale-110 cursor-pointer backdrop-blur">
+    <>
       {userData?.role !== Role.BANNED &&
-        <Link href="/register">레슨 신청하기</Link>
+        <div className="border-white border-2 text-xl mobile:px-4 mobile:py-2 px-8 py-4 transition-all hoverable:hover:scale-110 active:scale-110 cursor-pointer backdrop-blur">
+          <Link href="/register">레슨 신청하기</Link>
+        </div>
       }
-    </div>
+    </>
   );
 }
