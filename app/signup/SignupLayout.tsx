b3876653ada836fcc92d6b760c531a5cd3d51561
CubeDr <spaceship00@naver.com>
2024-10-08 01:38:30 +0900

    Limit max width for login & signup

--- app/signup/SignupLayout.tsx
+++ app/signup/SignupLayout.tsx
@@ -29,7 +29,7 @@ export default function SignupLayout({ children }: PropsWithChildren) {
 
   return (
     <main className="flex flex-col items-center justify-center p-8">
-      <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+      <div className="w-[420px] bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
             회원 가입
