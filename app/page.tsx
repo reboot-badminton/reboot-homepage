7c3c5704e57a7fca31b8ca161dd8f34a9b4a1cc2
CubeDr <spaceship00@naver.com>
2024-10-08 23:41:10 +0900

    Add naver site verification

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import ImageSlide from '@/app/components/ImageSlide';
 import Introduction from './components/Introduction';
 import MainCoaches from './components/MainCoaches';
 import Members from './components/Members';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  other: {
+    'naver-site-verification': '7afbf4c1c8db9d882054d33685a1e4454790e526',
+  },
+};
 
 const slideImages = Array.from({ length: 4 }, (_, index) => ({
   src: `/slide/${index}.webp`,
